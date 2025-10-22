b9d875468faab761785d912337edb40c0a8240cc
aaron <delandtree@gmail.com>
2025-10-21 22:33:33 -0700

    Added comments & tweaked a reset bug

--- Beer Timer/src/defines.h
+++ Beer Timer/src/defines.h
@@ -3,15 +3,20 @@
 #include <Bounce2.h>
 #include <Timer.h>
 
+//Timers
 Chrono watch;
-MoToTimer chase,hueTimer;
+MoToTimer chase,hueTimer,btnDelay;
 
+//Button
 Bounce2::Button resetBtn = Bounce2::Button();
 
+//You can change pins here if you want
 #define ledPin 3
 #define reedPin A1
 #define resetPin 4
 
+//These can be adjsuted if you add or subtract the amount of displays
+//The naming scheme isnt my best 
 const uint8_t numOfSegments = 4;
 const uint8_t segLeds = 3;
 
@@ -22,17 +27,9 @@ const uint8_t numLeds (7 * segLeds * numOfSegments);
 const uint8_t numLedsSegment (7 * segLeds);
 
 //create the full strip of leds
-CRGB fullStrip[numLeds];
-
-
-//Set this RGB value to set the colors of the segments
-#define On CRGB( 255, 0, 0)
-
-//This will set the segments off;
-#define Off  CRGB( 0, 0, 0)
-
-
-bool handlePulled = false;
+//This will set the led amount based on the segments
+//then add one extra for the decimal place.
+CRGB fullStrip[numLeds+1];
 
 
 //segment counters
@@ -42,38 +39,62 @@ uint8_t first = 0;
 uint8_t second = 0;
 uint8_t third = 0;
 uint8_t fourth = 0;
-
-
 uint8_t chaseCount = 0;
 uint8_t hue = 0;
 
 
 //These hold the values to check if we won
 //values check from left to right
-const uint8_t win1 = 0;
-const uint8_t win2 = 0;
-const uint8_t win3 = 1;
-const uint8_t win4 = 4;
+
+//game logic
 bool checkCount = 0;
 bool inGame = 0;
 bool needsReset = 0;
 
+//Used to track the handle movement
+bool handlePulled = false;
 uint8_t handleReturn = 0;
+
 uint8_t winValue = 3; //0 = fail; 1 = win; 3 = nothing;
+
+
+
+/*********************************************************
+These are things you can edit for your vibes that you want
+***********************************************************/
+
+//Set this RGB value to set the colors of the segments
+#define On CRGB( 255, 0, 0)
+
+//This will set the segments off;
+#define Off  CRGB( 0, 0, 0)
 
 //How fast the LED moves
 uint8_t chaseTime = 25;
+
 //How fast the LED shifts colors
 uint8_t hueTime = 50;
+
 //how fast the red chases
 uint8_t failChase = 40;
-//how instense the fade effect is on the fail sequence
+
+//how intense the fade effect is on the fail sequence
 //50-80 seems to be the sweet spot
 //large the numer the shorter the tail
 uint8_t fade = 60;
 
+//set to 00.14 so I could test with only two displays
+const uint8_t win1 = 0;
+const uint8_t win2 = 0;
+const uint8_t win3 = 1;
+const uint8_t win4 = 4;
+
+
+//You shouldn't need to edit any of this
+//It handles breaking up the segments of the singular strip
+
 //These will break up the full strip into logical sections
-//create pointers for 1 segment
+//create pointers for 1st display segments
 CRGB* segOne = &fullStrip[0]; //0-2
 CRGB* segTwo = &fullStrip[segLeds * 1]; // 3-5
 CRGB* segThree = &fullStrip[segLeds * 2]; // 6-8
@@ -82,7 +103,7 @@ CRGB* segFive = &fullStrip[segLeds * 4]; //12-14
 CRGB* segSix = &fullStrip[segLeds * 5]; //15-17
 CRGB* segSeven = &fullStrip[segLeds * 6]; //18-20
 
-//create pointers for 2 segment
+//create pointers for 2nd display segments
 CRGB* oneSegOne = &fullStrip[segLeds * 7]; //21-23
 CRGB* oneSegTwo = &fullStrip[segLeds * 8]; //24-26
 CRGB* oneSegThree = &fullStrip[segLeds * 9]; //27-29
@@ -91,7 +112,7 @@ CRGB* oneSegFive = &fullStrip[segLeds * 11]; //33-35
 CRGB* oneSegSix = &fullStrip[segLeds * 12]; //36-38
 CRGB* oneSegSeven = &fullStrip[segLeds * 13]; //39-41
 
-//create pointers for 3 segment
+//create pointers for 3rd display segments
 CRGB* twoSegOne = &fullStrip[segLeds * 14]; 
 CRGB* twoSegTwo = &fullStrip[segLeds * 15]; 
 CRGB* twoSegThree = &fullStrip[segLeds * 16];
@@ -100,7 +121,7 @@ CRGB* twoSegFive = &fullStrip[segLeds * 18];
 CRGB* twoSegSix = &fullStrip[segLeds * 19];
 CRGB* twoSegSeven = &fullStrip[segLeds * 20];
 
-//create pointers for 4 segment
+//create pointers for 4th display segments
 CRGB* threeSegOne = &fullStrip[segLeds * 21]; 
 CRGB* threeSegTwo = &fullStrip[segLeds * 22]; 
 CRGB* threeSegThree = &fullStrip[segLeds * 23];
@@ -109,9 +130,9 @@ CRGB* threeSegFive = &fullStrip[segLeds * 25];
 CRGB* threeSegSix = &fullStrip[segLeds * 26];
 CRGB* threeSegSeven = &fullStrip[segLeds * 27];
 
-//Create 4 full segment strips
-CRGB* seg1 = &fullStrip[0];
-CRGB* seg2 = &fullStrip[numLedsSegment];
+//Create seperate pointers for each display
+CRGB* seg1 = &fullStrip[0]; //0-20
+CRGB* seg2 = &fullStrip[numLedsSegment]; //21-40
 CRGB* seg3 = &fullStrip[numLedsSegment * 2];
 CRGB* seg4 = &fullStrip[numLedsSegment * 3];
 
--- Beer Timer/src/main.cpp
+++ Beer Timer/src/main.cpp
@@ -4,18 +4,24 @@
 
 void setup() 
 {
-
+  //Set pins
   pinMode(ledPin, OUTPUT);
   pinMode(reedPin, INPUT_PULLUP);
 
+  //Create button
   resetBtn.attach(resetPin, INPUT_PULLUP);
   resetBtn.setPressedState(LOW);
   resetBtn.interval(5);
 
   //ignore if it errors, it compiles just fine.
   FastLED.addLeds<WS2812, ledPin, GRB>(fullStrip, numLeds); 
-  FastLED.setBrightness(255);
-
+  
+  //If you want to adjust how much voltage is being pulled
+  //Your model uses 12V @ 4A for the full strip.
+  //Basically you can software control the amp draw by dimming the LEDS.
+  //FastLED.setMaxPowerInVoltsAndMilliamps(12,4000)
+  
+  //There is some Debug info
   Serial.begin(9600);
   
   Serial.print("total Leds: ");
@@ -25,9 +31,15 @@ void setup()
   Serial.println(segLeds);
   watch.start();
   
-  //Set Leds to black on restart
+  //Clear any data from LEDS
   FastLED.clear();
   FastLED.show();
+
+  //Set Leds to 0
+  setOneDisplayNum(first);
+  setTwoDisplayNum(second);
+  setThirdDisplayNum(third);
+  setFourthDisplayNum(fourth);
 
 }
 
@@ -36,17 +48,25 @@ void win()
   if(!chase.running())
   {  
 
-    //This has a smoother transtions between colors
-    // fill_rainbow(seg1,chaseCount,hue,5);
-    // fill_rainbow(seg2,chaseCount,hue,5);
-    // fill_rainbow(seg3,chaseCount,hue,5);
-    // fill_rainbow(seg4,chaseCount,hue,5);
+    //uncomment or comment this to see which one you like better.
 
-    //This is a less smooth transtion almost like sprinkles
-    fill_rainbow_circular(seg1,chaseCount,hue);
-    fill_rainbow_circular(seg2,chaseCount,hue);
-    fill_rainbow_circular(seg3,chaseCount,hue);
-    fill_rainbow_circular(seg4,chaseCount,hue);
+    //#define smooth
+  
+    #if defined(smooth)
+      //This has a smoother transtions between colors
+      fill_rainbow(seg1,chaseCount,hue,5);
+      fill_rainbow(seg2,chaseCount,hue,5);
+      fill_rainbow(seg3,chaseCount,hue,5);
+      fill_rainbow(seg4,chaseCount,hue,5);
+
+    #else
+      //This forces the rainbow spectrum based on LED count
+      //It makes it less smooth with small strips and more like sprinkles of colors
+      fill_rainbow_circular(seg1,chaseCount,hue);
+      fill_rainbow_circular(seg2,chaseCount,hue);
+      fill_rainbow_circular(seg3,chaseCount,hue);
+      fill_rainbow_circular(seg4,chaseCount,hue);
+    #endif
 
     chase.setTime(chaseTime);
     chaseCount++;
@@ -89,6 +109,7 @@ void fail()
 }
 
 
+
 //Check and return if sensor is triggered or not
 bool checkSensor()
 {
@@ -121,7 +142,7 @@ void startCount()
     //When we hit 10 it rolls the next number up.
     
     //we will have to guess what we want the timing to be
-    //This is going to go fast
+    //This is going to go fast if it is a ture timer
     if(watch.hasPassed(300))
     {
       first++;
@@ -160,6 +181,7 @@ void startCount()
 
 void resetLeds()
 {
+  //resets when the button is pressed to clear out and start over
   first = 0;
   second = 0;
   third = 0;
@@ -170,10 +192,9 @@ void resetLeds()
   setThirdDisplayNum(third);
   setFourthDisplayNum(fourth);
 
-  winValue = 0;
-
+  inGame = 0;
+  winValue = 3;
   handleReturn = 0;
-
   needsReset = 0;
 }
 
@@ -183,15 +204,17 @@ void loop()
 
   //Set the handle state
   //We might need to add logic to account for "feathering" the handle
+  //We are reading the reed digital but we can read analog to cerate a threshold if needing to.
   //will have to test against the real setup
 
   handlePulled = checkSensor();
-  // Serial.print("Handle State: ");
-  // Serial.println(handlePulled);
+  Serial.print("Handle State: ");
+  Serial.println(handlePulled);
 
   Serial.print("Handle Return: ");
   Serial.println(handleReturn);  
 
+  //only count while we are in game and not needing to reset
   if(!inGame)
   {   
    if(handlePulled) 
@@ -206,14 +229,16 @@ void loop()
     }
   }
 
+  //The handle has been returned
+  //This is when we will check if they got it correct
   if(handleReturn == 2)
   {
     checkCount = 1;
-    //increment forward to prevent a double check
     needsReset = 1;
     handleReturn = 3;
   }
   
+  //Check the stored values to the timer values 
   if(checkCount)
   {
     if(fourth == win1 && third == win2 && second == win3 && first == win4)
@@ -223,13 +248,16 @@ void loop()
     }
     else
     {
+      //set to play fail
       winValue = 0;
     }
 
+    //exit the game and leave timer checking
     inGame = 0;
     checkCount = 0;
   }
 
+  //if have a winValue other than default play the right sequence
 
   if(winValue !=3)
   {
@@ -244,11 +272,12 @@ void loop()
   }
 
 
+  //Check to see if the reset button was pressed
   resetBtn.update();
-
-  if(resetBtn.isPressed())
+  if(resetBtn.isPressed() && !btnDelay.running())
   {
     resetLeds();
+    btnDelay.setTime(300);
   }
   
 }
